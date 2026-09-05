530f54cb3f72f0133a23fe34dc2c937034f8ca39
Naveen-@5783 <navlakshminag5783@gmail.com>
2026-09-05 16:29:09 +0530

    Delete OneDrive/Desktop directory

--- OneDrive/Desktop/products_exp/backend/routes/feedbackRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createFeedback, getFeedbacks } = require('../controllers/feedbackController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.get('/', getFeedbacks);
-router.post('/', protect, createFeedback);
-
-module.exports = router;
